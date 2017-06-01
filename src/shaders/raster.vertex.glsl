d5023c4c9f697357a6c420c9a1c405d1439e04f8
Molly Lloyd <molly@mapbox.com>
2017-06-01 15:49:37 +0200

    grammar :police_car:

--- src/shaders/raster.vertex.glsl
+++ src/shaders/raster.vertex.glsl
@@ -11,10 +11,10 @@ varying vec2 v_pos1;
 
 void main() {
     gl_Position = u_matrix * vec4(a_pos, 0, 1);
-    // we are using Int16 for texture position coordinates to give us enough precision for
-    // fractional coordinates. we use 8192 to scale the texture coordinates in the buffer
-    // as an arbitrarily high number to preserve adequare precision when rendering
-    // this is also the same value as the EXTENT we are using for our tile buffer pos coordinates,
+    // We are using Int16 for texture position coordinates to give us enough precision for
+    // fractional coordinates. We use 8192 to scale the texture coordinates in the buffer
+    // as an arbitrarily high number to preserve adequate precision when rendering.
+    // This is also the same value as the EXTENT we are using for our tile buffer pos coordinates,
     // so math for modifying either is consistent.
     v_pos0 = (((a_texture_pos / 8192.0) - 0.5) / u_buffer_scale ) + 0.5;
     v_pos1 = (v_pos0 * u_scale_parent) + u_tl_parent;
